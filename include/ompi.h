83e6269d1e42bb9e6e683bfebd600b4af11bc9ef
zat2 Florian Weidle <zat2@ad6-101.bullx>
2022-12-05 12:33:46 +0000

    Bugfixes in 001 and transfer.
    Some bugfixes in transfer task and some adaptations in 001 and ompi
    due to upgrade of SLURM.

--- include/ompi.h
+++ include/ompi.h
@@ -3,5 +3,5 @@ module load prgenv/intel
 module load intel/2021.4.0    
 module load intel-mkl/19.0.5  
 module load hpcx-openmpi/2.9.0
-module load gcc/8.4.1         
+module load gcc
 module load ecmwf-toolbox/2021.08.3.0
